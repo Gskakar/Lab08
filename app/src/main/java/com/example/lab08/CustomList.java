2f2bf3ab71782ff1b3f046ba134f58c53756f6bb
Gurkeerat Kakar <gskakar@ualberta.ca>
2025-10-29 13:55:10 -0600

    More Failing Tests

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -16,8 +16,4 @@ public class CustomList {
 
     public boolean hasCity(City city) { return cities.contains(city);}
 
-    
-
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -8,7 +8,24 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-
         assertTrue(list.hasCity(calgary));
+    }
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.getCount());
+        list.addCity(new City("Edmonton", "AB"));
+        list.addCity(new City("Calgary", "AB"));
+        assertEquals(2, list.getCount());
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
+        list.deleteCity(edmonton);
+        assertEquals(0, list.getCount());
+        assertThrows(IllegalArgumentException.class, () -> list.deleteCity(edmonton));
     }
 }
